21fef235a51c6146900c2c99893ac89c12a80bfd
Lucas Brigido <lucasbrigido@MacBook-Air-de-Lucas.local>
2025-06-13 20:30:24 -0300

    fix vercel url in fetch

--- src/modules/users/client.js
+++ src/modules/users/client.js
@@ -1,7 +1,10 @@
 
 class UserClient {
+    constructor () {
+        this._baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+    }
     async userInfo() {
-        const res = await fetch('http://localhost:3000/api/users')
+        const res = await fetch(`${this._baseUrl}/api/users`)
         if (!res.ok) throw new Error('Falha ao buscar usuários')
         return res.json()
     }
