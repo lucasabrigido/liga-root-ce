28d8875cb8f372f844793c064ecaab06e0d44fc2
Lucas Brigido <lucasbrigido@MacBook-Air-de-Lucas.local>
2025-06-15 14:27:30 -0300

    fix date

---
+++ src/components/date-picker/index.js
@@ -0,0 +1,42 @@
+'use client';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import styles from './date-picker.module.scss';
+import 'dayjs/locale/pt-br';
+
+dayjs.locale('pt-br');
+
+export default function BasicDatePicker({ name, placeholder, ...rest }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
+      <DatePicker
+        className={styles.datePicker}
+        format="DD/MM/YYYY"
+        {...rest}
+        slotProps={{
+          textField: {
+            variant: 'outlined',
+            InputLabelProps: { shrink: false },
+            inputProps: {
+              placeholder,
+              readOnly: false,
+              name,
+            },
+          },
+        }}
+      />
+    </LocalizationProvider>
+  );
+}
