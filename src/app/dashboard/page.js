6fc7eb258cbf3872b429139a3bae243ce1351c6f
Lucas Brigido <lucasbrigido@MacBook-Air-de-Lucas.local>
2025-06-16 23:33:43 -0300

    dashboard5

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,22 +1,41 @@
-import { use } from 'react';
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
 import { UserStatsTable } from './stats-table';
-import UserClient from '@modules/users/client';
-import styles from './styles.module.scss';
 import { FactionPieChart } from './stats-faction';
+import CircularProgress from '@mui/material/CircularProgress';
+import UserClient from '@modules/users/client';
 import { ROOT_FACTIONS_MAP } from '@utils/constants';
+import styles from './styles.module.scss';
 
 export default function Dashboard() {
-    const stats = use(UserClient.listGames());
-    
+  const { data: stats, isLoading, isError } = useQuery({
+    queryKey: ['userStats'],
+    queryFn: () => UserClient.listGames(),
+  });
+
+  if (isLoading) {
+    return (
+      <div className={styles.loadingWrapper}>
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (isError || !stats) {
+    return <p style={{ color: 'red' }}>Erro ao carregar estatísticas.</p>;
+  }
+
+  const userIds = Object.keys(stats.totalPointsByUser || {});
+  const factionData = Object.keys(stats.totalGamesByFaction || {}).map((faction) => ({
+    faction: ROOT_FACTIONS_MAP[faction],
+    totalGames: stats.totalGamesByFaction[faction],
+  }));
+
   return (
     <div className={styles.page}>
-        <UserStatsTable stats={stats} data={Object.keys(stats?.totalPointsByUser || {})}/>
-        <FactionPieChart data={Object.keys(stats.totalGamesByFaction || {}).map(faction => {
-            return {
-                faction: ROOT_FACTIONS_MAP[faction],
-                totalGames: stats.totalGamesByFaction[faction],
-            }
-        })}/>
+      <UserStatsTable stats={stats} data={userIds} />
+      <FactionPieChart data={factionData} />
     </div>
   );
 }
